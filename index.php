3d352972241f75e516cf29251f872b587e0e2fe2
Mohiuddin Abdul Kader <muhin.cse.diu@gmail.com>
2021-08-30 22:24:14 +0600

    Retrieve latitude and longitude
    Created a function to retrieve latitude and longitude code by passing the target location.

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
+<?php
+
+function getGeoCode($address)
+{
+    $api_key = 'DIzaSeDMazf9vLhu5yjnQu16O-4dDcaYQws9-Zs';
+    $address = str_replace(' ', '+', $address);
+    $url = 'https://maps.google.com/maps/api/geocode/json?address='.$address.'&key='.$api_key.'';
+    $curlSession = curl_init();
+    curl_setopt($curlSession, CURLOPT_URL, $url);
+    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+    $jsonData = curl_exec($curlSession);
+    curl_close($curlSession);
+    $json = json_decode($jsonData);
+	  $data['lat'] = $json->results[0]->geometry->location->lat;
+	  $data['lng'] = $json->results[0]->geometry->location->lng;
+	  return $data;
+}
+
+
+
 
